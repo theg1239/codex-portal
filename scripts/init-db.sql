4ab151a77293a7de75f21de9ca234d4447a76c17
theg <52027622+theg1239@users.noreply.github.com>
2025-06-07 15:18:48 +0530

    fix: update .gitignore to include .env and update package-lock.json for pg and dotenv versions

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -7,11 +7,11 @@ CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    difficulty VARCHAR(10) CHECK (difficulty IN ('easy', 'medium', 'hard')) NOT NULL,
-    answer TEXT NOT NULL,
+    difficulty VARCHAR(10) CHECK (difficulty IN ('easy', 'medium', 'hard')) NOT NULL,    answer TEXT NOT NULL,
     must_include TEXT
+);
 
-CREATE TABLE submissions (
+CREATE TABLE submissions(
     id SERIAL PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
     question_id INTEGER REFERENCES questions(id),
